9b0159837a46851fe56650c9c514c43e92ce2927
Juniper-Tang <juniper32616@outlook.com>
2026-07-16 14:25:21 +0700

    Fix Panyuxuan course status 2026-07-16

---
+++ archive-access-config.js
@@ -0,0 +1,6 @@
+window.JILI_ARCHIVE_ACCESS_CONFIG = {
+  enabled: true,
+  endpoint: "https://1439604325-k2xvyt4tit.ap-guangzhou.tencentscf.com",
+  credentials: "omit",
+  localArchiveEnabled: false
+};
